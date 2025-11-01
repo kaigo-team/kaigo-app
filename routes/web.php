922ce78ed463f30bad3e1869ae2de2412c9d86bf
twbc07 <twbc07@gmail.com>
2025-11-01 15:31:09 +0900

    Renderデプロイのための設定

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,13 @@ use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 use Livewire\Volt\Volt;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
-
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
 // 要介護度算出アプリのルート
-Volt::route('kaigo/input', 'kaigo.input')->name('kaigo.input');
-Volt::route('kaigo/result/{input}', 'kaigo.result')->name('kaigo.result');
+Volt::route('/', 'kaigo/input', 'kaigo.input')->name('kaigo.input');
+Volt::route('/','kaigo/result/{input}', 'kaigo.result')->name('kaigo.result');
 
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
